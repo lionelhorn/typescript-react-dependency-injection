e833259a0456d3b9a8cafeba6bb9b44ae6136321
Lionel Horn <dev@lionelhorn.com>
2024-09-22 14:43:29 +0200

    refactor: use non swc plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { deepkitType } from "@deepkit/vite";
 
 // https://vitejs.dev/config/
